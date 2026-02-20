5f5e94c474c4dd8d4c2be3ef35e254bffb3e22ff
Dennis Fölster <dennis.foelster@ffw.com>
2026-02-20 14:23:22 +0100

    Add TvOS support to the auth module.
    There are some parts of the public api, that are not existing in the TvOS sdk.
    Mostly relating to auth via phone number or multifactor.
    Those methods will throw an UnsupportedOperationException with this change.

--- firebase-auth/src/appleMain/kotlin/dev/gitlive/firebase/auth/auth.kt
+++ firebase-auth/src/appleMain/kotlin/dev/gitlive/firebase/auth/auth.kt
@@ -8,8 +8,6 @@ import cocoapods.FirebaseAuth.*
 import dev.gitlive.firebase.Firebase
 import dev.gitlive.firebase.FirebaseApp
 import dev.gitlive.firebase.FirebaseException
-import dev.gitlive.firebase.FirebaseNetworkException
-import dev.gitlive.firebase.FirebaseTooManyRequestsException
 import dev.gitlive.firebase.auth.ActionCodeResult.*
 import dev.gitlive.firebase.ios
 import kotlinx.cinterop.*
@@ -167,10 +165,11 @@ public actual open class FirebaseAuthEmailException(message: String, code: Strin
 public actual open class FirebaseAuthInvalidCredentialsException(message: String, code: String? = null) : FirebaseAuthException(message, code)
 public actual open class FirebaseAuthWeakPasswordException(message: String, code: String? = null) : FirebaseAuthInvalidCredentialsException(message, code)
 public actual open class FirebaseAuthInvalidUserException(message: String, code: String? = null) : FirebaseAuthException(message, code)
-public actual open class FirebaseAuthMultiFactorException(message: String, public val resolver: FIRMultiFactorResolver?, code: String? = null) : FirebaseAuthException(message, code)
 public actual open class FirebaseAuthRecentLoginRequiredException(message: String, code: String? = null) : FirebaseAuthException(message, code)
 public actual open class FirebaseAuthUserCollisionException(message: String, code: String? = null) : FirebaseAuthException(message, code)
 public actual open class FirebaseAuthWebException(message: String, code: String? = null) : FirebaseAuthException(message, code)
+
+internal expect fun NSError.toAuthException(): FirebaseException
 
 internal fun <T, R> T.throwError(block: T.(errorPointer: CPointer<ObjCObjectVar<NSError?>>) -> R): R {
     memScoped {
@@ -178,7 +177,7 @@ internal fun <T, R> T.throwError(block: T.(errorPointer: CPointer<ObjCObjectVar<
         val result = block(errorPointer)
         val error: NSError? = errorPointer.pointed.value
         if (error != null) {
-            throw error.toException()
+            throw error.toAuthException()
         }
         return result
     }
@@ -190,7 +189,7 @@ internal suspend inline fun <T, reified R> T.awaitResult(function: T.(callback: 
         if (error == null) {
             job.complete(result)
         } else {
-            job.completeExceptionally(error.toException())
+            job.completeExceptionally(error.toAuthException())
         }
     }
     return job.await() as R
@@ -202,71 +201,8 @@ internal suspend inline fun <T> T.await(function: T.(callback: (NSError?) -> Uni
         if (error == null) {
             job.complete(Unit)
         } else {
-            job.completeExceptionally(error.toException())
+            job.completeExceptionally(error.toAuthException())
         }
     }
     job.await()
-}
-
-private fun NSError.toException() = when (domain) {
-    // codes from AuthErrors.swift: https://github.com/firebase/firebase-ios-sdk/blob/
-    // 2f6ac4c2c61cd57c7ea727009e187b7e1163d613/FirebaseAuth/Sources/Swift/Utilities/
-    // AuthErrors.swift#L51
-    FIRAuthErrorDomain -> when (code) {
-        17030L, // AuthErrorCode.invalidActionCode
-        17029L, // AuthErrorCode.expiredActionCode
-        -> FirebaseAuthActionCodeException(toString(), code.toString())
-
-        17008L, // AuthErrorCode.invalidEmail
-        -> FirebaseAuthEmailException(toString(), code.toString())
-
-        17056L, // AuthErrorCode.captchaCheckFailed
-        17042L, // AuthErrorCode.invalidPhoneNumber
-        17041L, // AuthErrorCode.missingPhoneNumber
-        17046L, // AuthErrorCode.invalidVerificationID
-        17044L, // AuthErrorCode.invalidVerificationCode
-        17045L, // AuthErrorCode.missingVerificationID
-        17043L, // AuthErrorCode.missingVerificationCode
-        17021L, // AuthErrorCode.userTokenExpired
-        17004L, // AuthErrorCode.invalidCredential
-        17009L, // AuthErrorCode.wrongPassword
-        -> FirebaseAuthInvalidCredentialsException(toString(), code.toString())
-
-        17026L, // AuthErrorCode.weakPassword
-        -> FirebaseAuthWeakPasswordException(toString(), code.toString())
-
-        17017L, // AuthErrorCode.invalidUserToken
-        -> FirebaseAuthInvalidUserException(toString(), code.toString())
-
-        17014L, // AuthErrorCode.requiresRecentLogin
-        -> FirebaseAuthRecentLoginRequiredException(toString(), code.toString())
-
-        17087L, // AuthErrorCode.secondFactorAlreadyEnrolled
-        17078L, // AuthErrorCode.secondFactorRequired
-        17088L, // AuthErrorCode.maximumSecondFactorCountExceeded
-        17084L, // AuthErrorCode.multiFactorInfoNotFound
-        -> {
-            val resolver = userInfo["FIRAuthErrorUserInfoMultiFactorResolverKey"] as? FIRMultiFactorResolver
-            FirebaseAuthMultiFactorException(toString(), resolver, code.toString())
-        }
-
-        17052L, // AuthErrorCode.quotaExceeded
-        -> FirebaseTooManyRequestsException(toString())
-
-        17007L, // AuthErrorCode.emailAlreadyInUse
-        17012L, // AuthErrorCode.accountExistsWithDifferentCredential
-        17025L, // AuthErrorCode.credentialAlreadyInUse
-        -> FirebaseAuthUserCollisionException(toString(), code.toString())
-
-        17057L, // AuthErrorCode.webContextAlreadyPresented
-        17058L, // AuthErrorCode.webContextCancelled
-        17062L, // AuthErrorCode.webInternalError
-        -> FirebaseAuthWebException(toString(), code.toString())
-
-        17020L, // AuthErrorCode.networkError
-        -> FirebaseNetworkException(toString())
-
-        else -> FirebaseAuthException(toString(), code.toString())
-    }
-    else -> FirebaseAuthException(toString())
 }
---
+++ firebase-auth/src/tvosMain/kotlin/dev/gitlive/firebase/auth/auth.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2020 GitLive Ltd.  Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package dev.gitlive.firebase.auth
+
+import cocoapods.FirebaseAuth.FIRAuthErrorDomain
+import dev.gitlive.firebase.FirebaseNetworkException
+import dev.gitlive.firebase.FirebaseTooManyRequestsException
+import platform.Foundation.NSError
+
+public actual open class FirebaseAuthMultiFactorException(message: String, code: String? = null) : FirebaseAuthException(message, code)
+
+internal actual fun NSError.toAuthException() = when (domain) {
+    // codes from AuthErrors.swift: https://github.com/firebase/firebase-ios-sdk/blob/
+    // 2f6ac4c2c61cd57c7ea727009e187b7e1163d613/FirebaseAuth/Sources/Swift/Utilities/
+    // AuthErrors.swift#L51
+    FIRAuthErrorDomain -> when (code) {
+        17030L, // AuthErrorCode.invalidActionCode
+        17029L, // AuthErrorCode.expiredActionCode
+        -> FirebaseAuthActionCodeException(toString(), code.toString())
+
+        17008L, // AuthErrorCode.invalidEmail
+        -> FirebaseAuthEmailException(toString(), code.toString())
+
+        17056L, // AuthErrorCode.captchaCheckFailed
+        17042L, // AuthErrorCode.invalidPhoneNumber
+        17041L, // AuthErrorCode.missingPhoneNumber
+        17046L, // AuthErrorCode.invalidVerificationID
+        17044L, // AuthErrorCode.invalidVerificationCode
+        17045L, // AuthErrorCode.missingVerificationID
+        17043L, // AuthErrorCode.missingVerificationCode
+        17021L, // AuthErrorCode.userTokenExpired
+        17004L, // AuthErrorCode.invalidCredential
+        17009L, // AuthErrorCode.wrongPassword
+        -> FirebaseAuthInvalidCredentialsException(toString(), code.toString())
+
+        17026L, // AuthErrorCode.weakPassword
+        -> FirebaseAuthWeakPasswordException(toString(), code.toString())
+
+        17017L, // AuthErrorCode.invalidUserToken
+        -> FirebaseAuthInvalidUserException(toString(), code.toString())
+
+        17014L, // AuthErrorCode.requiresRecentLogin
+        -> FirebaseAuthRecentLoginRequiredException(toString(), code.toString())
+
+        17052L, // AuthErrorCode.quotaExceeded
+        -> FirebaseTooManyRequestsException(toString())
+
+        17007L, // AuthErrorCode.emailAlreadyInUse
+        17012L, // AuthErrorCode.accountExistsWithDifferentCredential
+        17025L, // AuthErrorCode.credentialAlreadyInUse
+        -> FirebaseAuthUserCollisionException(toString(), code.toString())
+
+        17057L, // AuthErrorCode.webContextAlreadyPresented
+        17058L, // AuthErrorCode.webContextCancelled
+        17062L, // AuthErrorCode.webInternalError
+        -> FirebaseAuthWebException(toString(), code.toString())
+
+        17020L, // AuthErrorCode.networkError
+        -> FirebaseNetworkException(toString())
+
+        else -> FirebaseAuthException(toString(), code.toString())
+    }
+    else -> FirebaseAuthException(toString())
+}
